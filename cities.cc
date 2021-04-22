3d12f8b2a1001b20e76398e29a458c154a46358c
nichollsharrisonj <66330102+nichollsharrisonj@users.noreply.github.com>
2021-04-22 01:01:00 -0700

    fixed distance function

--- cities.cc
+++ cities.cc
@@ -13,23 +13,23 @@ Cities::Cities(std::vector<coord_t> citiesvector){
 }
 
 //find the distance between two coord_t objects
-unsigned distance_between_coords(Cities::coord_t city_one,Cities::coord_t city_two){
-	int xdist = (city_one.first - city_two.first);
-	int ydist = (city_one.second - city_two.second);
+double distance_between_coords(Cities::coord_t city_one,Cities::coord_t city_two){
+	double xdist = (city_one.first - city_two.first);
+	double ydist = (city_one.second - city_two.second);
 	return sqrt(pow(xdist,2) + pow(ydist,2));
 }
 
 //Find the total path distance
 double 
 Cities::total_path_distance(const Cities::permutation_t& ordering) const{
-	unsigned distance = 0;
-	for (unsigned i = 0; i < ordering.size() - 2; i++){ //iterate through permutation vector, stopping at 2nd to last element
-		unsigned nextstep = distance_between_coords(cities_list_[i],cities_list_[i+1]);
+	double distance = 0;
+	for (unsigned i = 0; i < cities_list_.size() - 1; i++){ //iterate through permutation vector, stopping at 2nd to last element
+		double nextstep = distance_between_coords(cities_list_[ordering[i]],cities_list_[ordering[i+1]]);
 		distance += nextstep;
 	}
 	//add the distance from the last city to the first city in the permutation
 	coord_t first_city = cities_list_[ordering[0]];
-	coord_t last_city = cities_list_[ordering[ordering.size() - 1]];
+	coord_t last_city = cities_list_[ordering.back()];
 	distance += distance_between_coords(first_city,last_city);
 	return distance;
 }
@@ -47,9 +47,11 @@ Cities::reorder(const Cities::permutation_t& ordering) const{
 //overload >> operator
 std::istream 
 &operator>> (std::istream& input, Cities& cities) {
- 	Cities::coord_t newcoord;
-	input >> newcoord.first >> newcoord.second;
-	cities.cities_list_.push_back(newcoord);
+ 	while (!input.eof()){
+	 	Cities::coord_t newcoord;
+		input >> newcoord.first >> newcoord.second;
+		cities.cities_list_.push_back(newcoord);
+	}
 	return input;            
 }
 
@@ -58,7 +60,7 @@ std::ostream
 &operator<< (std::ostream& output, Cities& cities) { 
 	// std::string outputstring;
      for (Cities::coord_t coords : cities.cities_list_){
-     	output << coords.first << coords.second;
+     	output << " x: " << coords.first << " y: " << coords.second << std::endl;
      }
      return output;            
 }
@@ -66,10 +68,19 @@ std::ostream
 
 
 int main(){
+	//Eitan provided this test code
 	auto fin = std::ifstream("five.tsv");
 	Cities cities;
 	fin >> cities;
+	std::cout << cities << "\n";
 	std::cout << cities.total_path_distance({ 0, 1, 2, 3, 4 }) << "\n"; // Should be 48.8699
 	std::cout << cities.total_path_distance({ 3, 2, 4, 0, 1 }) << "\n"; // Should be 53.42.43
+
+	// auto fin = std::ifstream("linear.tsv");
+	// Cities cities;
+	// fin >> cities;
+	// std::cout << cities << "\n";
+	// std::cout << cities.total_path_distance({ 0, 1, 2}) << "\n"; // Should be 48.8699
+	// std::cout << cities.total_path_distance({ 1, 2, 0}) << "\n"; // Should be 53.42.43
 	return 1;
 }
