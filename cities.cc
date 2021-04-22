3237d7c4d040d34c5acbf366ca46f26993be3cb0
nichollsharrisonj <66330102+nichollsharrisonj@users.noreply.github.com>
2021-04-21 19:02:46 -0700

    Added distance_between_coords function to find distance between two cities

--- cities.cc
+++ cities.cc
@@ -5,6 +5,11 @@
 std::istream &operator>> (std::istream& input, Cities& cities) { 
 	input >> *cities;
 	return input;            
+//find the distance between two coord_t objects
+unsigned distance_between_coords(Cities::coord_t city_one,Cities::coord_t city_two){
+	int xdist = (city_one.first - city_two.first);
+	int ydist = (city_one.second - city_two.second);
+	return sqrt(pow(xdist,2) + pow(ydist,2));
 }
 //overload << operator
 std::ostream &operator<< (std::ostream& output, Cities& cities) { 
